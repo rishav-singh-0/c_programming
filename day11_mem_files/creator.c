d8b55caf9deb991b02d28382a809c521fd1a9bbc
rishav <rishav@no-reply.com>
2023-02-01 16:13:28 +0530

    added NULL check if file creation not possible

--- day11_mem_files/creator.c
+++ day11_mem_files/creator.c
@@ -3,8 +3,14 @@
 int main() {
     FILE* f1 = fopen("string.txt", "w");
 
+    // printf("%d", f1);
     char str[50] = {'a', 'b', '\0', 'c', 'd', -1, 'e', 'f'};
     
+    if(NULL==f1){
+        printf("File didn't open");
+        return 1;
+    }
+
     for(int i=0; i<50; i++){
         fputc(str[i], f1);
         printf("%d\t%c\n", str[i], str[i]);
