9d8a64bbbea9fe3222a3282cf9c6286fbce524a4
rishav <rishav@no-reply.com>
2023-02-06 18:21:00 +0530

    updated linklist code

--- task/add_linklists.c
+++ task/add_linklists.c
@@ -13,7 +13,7 @@ void printList(const ListNode *);
 void freeList(ListNode *);
 
 int main() {
-    ListNode* Num1 = populateList(342);
+    ListNode* Num1 = populateList(12342);
     ListNode* Num2 = populateList(465);
 
     ListNode* result = addTwoNumbers(Num1, Num2);
@@ -43,7 +43,7 @@ int lenList(const ListNode* iHead){
     int len = 0;
     const ListNode* ptr = iHead;
     for(len=0; NULL!=ptr; ptr=ptr->next, len++);
-    printf("%d\n", len);
+    // printf("%d\n", len);
     return len;
 }
 
@@ -58,55 +58,64 @@ void freeList(ListNode* iHead){
 }
 
 void printList(const ListNode* iHead){
-    const ListNode* tmp = iHead;
-
-    while ( NULL != tmp){
-        printf("%d ->", tmp->data);
-        tmp = tmp->next;
+    while ( NULL != iHead){
+        printf("%d ->", iHead->data);
+        iHead = iHead->next;
     }
     printf("NULL\n");
 }
 
 ListNode * addTwoNumbers(ListNode* iList1_Ptr, ListNode* iList2_Ptr){
 
-
     if( NULL ==iList1_Ptr){
         printf("[%s]:[%d] iList1_Ptr of Link List is pointing to NULL\n", __func__, 75);
-        return NULL ;
+        return NULL;
     }
     if( NULL ==iList2_Ptr){
         printf("[%s]:[%d] iList2_Ptr of Link List is pointing to NULL\n", __func__, 79);
-        return NULL ;
+        return NULL;
     }
 
     ListNode* newListPtr = (ListNode*)malloc(sizeof(ListNode));
-    ListNode* ptr1 = iList1_Ptr;
-    ListNode* ptr2 = iList2_Ptr;
     ListNode* result = newListPtr;
     int listLen1 = lenList(iList1_Ptr);
     int listLen2 = lenList(iList2_Ptr);
 
     int addition, carry = 0;
-    while( NULL !=ptr1 || NULL !=ptr2){
+    int iter = listLen1;
+    if(listLen2>iter){
+        iter = listLen2;
+    }
 
-        addition = carry + ptr1->data + ptr2->data;
+    int num1 = iList1_Ptr->data, num2 = iList2_Ptr->data;
+    while(0!=iter){
+        iter--;
+
+        addition = carry + num1 + num2;
         carry = addition/10;
         addition = addition%10;
-        printf("%d, %d\n", carry, addition);
+        // printf("%d, %d\n", carry, addition);
         result->data = addition;
-        result->next = (ListNode*)malloc(sizeof(ListNode));
 
+        if(iter){
+            result->next = (ListNode*)malloc(sizeof(ListNode));
+            result = result->next;
+        }
 
-            ptr1 = ptr1->next;
+        if(NULL != iList1_Ptr->next){
+            iList1_Ptr = iList1_Ptr->next;
+            num1 = iList1_Ptr->data;
+        }
+        else{ num1 = 0;}
 
-
-            ptr2 = ptr2->next;
-
-        result = result->next;
-
+        if(NULL != iList2_Ptr->next){
+            iList2_Ptr = iList2_Ptr->next;
+            num2 = iList2_Ptr->data;
+        }
+        else{ num2 = 0;}
     }
     free(result->next);
-    result = NULL ;
+    result = NULL;
 
     return newListPtr;
 }
