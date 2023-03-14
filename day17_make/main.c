1f4a873a0cc364282445fa3b245ad0a5aa1a728e
rishav <rishav@no-reply.com>
2023-03-14 16:52:47 +0530

    updated makefile to make library and binaries

--- day17_make/main.c
+++ day17_make/main.c
@@ -7,8 +7,8 @@ int main(){
     num1 = 10;
     num2 = 5;
 
-    printf("Add of %d and %d = %d", num1, num2, add_num(num1, num2));
-    printf("Sub of %d and %d = %d", num1, num2, sub_num(num1, num2));
+    printf("Add of %d and %d = %d\n", num1, num2, add_num(num1, num2));
+    printf("Sub of %d and %d = %d\n", num1, num2, sub_num(num1, num2));
     
     return 0;
 }
